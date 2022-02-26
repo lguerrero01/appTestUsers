f06367974940b2ae0be6bcc99d0604add245f440
Alejandro <luisguerrero12387@gmail.com>
2022-02-25 21:14:47 -0400

    adding routing and lazy Loading
    html navbar and footer added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-
-const routes: Routes = [];
+/////////////////
+// Components
+////////////////
+const routes: Routes = [
+  {
+    path: 'user',
+    loadChildren: () =>
+      import('src/app/components/user/user.module').then((m) => m.UserModule),
+  },
+  {
+    path: '**',
+    redirectTo: '/user',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
