48395c206e6060f6edb2702842c3ff7041ab0afb
Alejandro <luisguerrero12387@gmail.com>
2022-02-26 00:02:14 -0400

    ReactiveForms

--- src/app/components/user/create-user/create-user.component.ts
+++ src/app/components/user/create-user/create-user.component.ts
@@ -1,15 +1,51 @@
+import { UserService } from './../../../shared/services/user.service';
 import { Component, OnInit } from '@angular/core';
-
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-create-user',
   templateUrl: './create-user.component.html',
-  styleUrls: ['./create-user.component.css']
+  styleUrls: ['./create-user.component.css'],
 })
 export class CreateUserComponent implements OnInit {
+  /////////////
+  // Atributes
+  ////////////
+  public formUser: FormGroup = new FormGroup({
+    title: new FormControl('', [Validators.required]),
+    body: new FormControl('', Validators.required),
+  });
 
-  constructor() { }
+  constructor(
+    private userServices: UserService,
+    private router: Router,
+    private formBuilder: FormBuilder
+  ) {}
 
   ngOnInit(): void {
+    this.formUser = this.formBuilder.group({
+      title: ['', Validators.required],
+      body: ['', Validators.required],
+    });
   }
 
+  public validFields(field: string) {
+    return (
+      this.formUser.controls[field].errors &&
+      this.formUser.controls[field].touched
+    );
+  }
+
+  public createUser() {
+    console.log(this.formUser.value);
+    this.userServices.createUser(this.formUser.value).subscribe((res) => {
+      console.log('User created successfully!');
+      this.router.navigateByUrl('user');
+    });
+  }
 }
